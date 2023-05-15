0d0869f567123ab6c46d1ac6d48db360f3e122c7
mcvha6 <marco.viergever@gmail.com>
2023-05-15 19:53:52 +0200

    Functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,17 +1,19 @@
-prijzen={
-    "aardbei":3,
-    "vanille":4,
-    "chocoldade":5
-}
-import decimal
-aanbieding=round(decimal.Decimal(prijzen["aardbei"]*0.8),2)
-reclame_tekst=(f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}")
-reclame_tekst3=reclame_tekst.upper()
-reclame_tekst4=reclame_tekst3.split()
-# for el in reclame_tekst4:
-#    print(el.lower())
-for el in reclame_tekst4:
-    if len(el)<5:
-          print(el.lower())
-    else:
-       print(el)
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen={
+        "aardbei":3,
+        "vanille":4,
+        "chocoldade":5
+    }
+    import decimal
+    aanbieding=round(decimal.Decimal(prijzen["aardbei"]*0.8),2)
+    reclame_tekst=(f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}")
+    reclame_tekst4=reclame_tekst.split()
+    for el in reclame_tekst4:
+        if len(el)<5:
+            print(el.lower())
+        else:
+            print(el.upper())
+decoreer("Aanbieding")
+print_aanbieding()
