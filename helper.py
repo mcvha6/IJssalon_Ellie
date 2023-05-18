c32b65fd9e9d91e001a75b3c18d4642d45eb23cc
mcvha6 <marco.viergever@gmail.com>
2023-05-19 00:08:25 +0200

    Aanpassing na les 9

--- helper.py
+++ helper.py
@@ -1,7 +1,21 @@
 def decoreer(tekst=""):
+
     lengte=len(tekst)+4
     print()
     print(lengte*"*")
     print(f"* {tekst} *")
     print(lengte*"*")
     print()
+
+def fooi_pp(bedrag,personen):
+    try:
+        bedrag_pp=bedrag/personen
+    except:
+        bedrag_pp="??"
+    return f"Het bedrag dat ieder krijgt bedraagt, {bedrag_pp} euro"
+
+b = int(input("Welk bedrag zit er in de fooienpot? "))
+p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
+
+print(fooi_pp(b,p))
+
